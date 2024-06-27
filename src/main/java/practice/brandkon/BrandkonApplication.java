27f3f773e4bf8bb5bc8d9e0a553883e4e4030780
Yul Jeon <dalbitstream@gmail.com>
2024-06-27 10:58:43 +0900

    Initial commit

---
+++ src/main/java/practice/brandkon/BrandkonApplication.java
@@ -0,0 +1,13 @@
+package practice.brandkon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandkonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandkonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/brandkon/BrandkonApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.brandkon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandkonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
