25886dc6fae40b6cd39f9a4fb17a503c89079f4b
Yul Jeon <dalbitstream@gmail.com>
2024-06-27 12:09:10 +0900

    CategoryService 추가 및 CategoryRestController에 의존성 주입

--- src/main/java/practice/brandkon/category/CategoryRestController.java
+++ src/main/java/practice/brandkon/category/CategoryRestController.java
@@ -8,9 +8,11 @@ import java.util.List;
 @RestController
 public class CategoryRestController {
 
+    private final CategoryService categoryService;
     private final CategoryRepository categoryRepository;
 
-    public CategoryRestController(CategoryRepository categoryRepository) {
+    public CategoryRestController(CategoryService categoryService, CategoryRepository categoryRepository) {
+        this.categoryService = categoryService;
         this.categoryRepository = categoryRepository;
     }
 
---
+++ src/main/java/practice/brandkon/category/CategoryService.java
@@ -0,0 +1,7 @@
+package practice.brandkon.category;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
