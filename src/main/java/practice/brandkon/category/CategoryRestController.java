d852b7351d6f19616a3c60559cb7a6f939fe9900
Yul Jeon <dalbitstream@gmail.com>
2024-06-27 12:11:23 +0900

    CategoryRestController에서 CategoryService로 목록 조회 책임을 옮김

--- src/main/java/practice/brandkon/category/CategoryRestController.java
+++ src/main/java/practice/brandkon/category/CategoryRestController.java
@@ -9,21 +9,13 @@ import java.util.List;
 public class CategoryRestController {
 
     private final CategoryService categoryService;
-    private final CategoryRepository categoryRepository;
 
-    public CategoryRestController(CategoryService categoryService, CategoryRepository categoryRepository) {
+    public CategoryRestController(CategoryService categoryService) {
         this.categoryService = categoryService;
-        this.categoryRepository = categoryRepository;
     }
 
     @GetMapping("/categories")
     public List<CategoryResponse> findAll() {
-        return categoryRepository.findAll()
-                .stream()
-                .map(c -> new CategoryResponse(
-                        c.getId(),
-                        c.getImageUrl(),
-                        c.getName()))
-                .toList();
+        return categoryService.findAll();
     }
 }
--- src/main/java/practice/brandkon/category/CategoryService.java
+++ src/main/java/practice/brandkon/category/CategoryService.java
@@ -2,6 +2,24 @@ package practice.brandkon.category;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(c -> new CategoryResponse(
+                        c.getId(),
+                        c.getImageUrl(),
+                        c.getName()))
+                .toList();
+    }
 }
