9fa95aeef3949ad36c90456941bfd983b3133011
Yul Jeon <dalbitstream@gmail.com>
2024-07-04 17:05:51 +0900

    동시성 문제 해결: 동일 상품에 대한 구매 요청 여러 개가 동시에 들어오면 구매 개수가 제대로 증가하지 않는 문제

--- src/main/java/practice/brandkon/product/ProductRepository.java
+++ src/main/java/practice/brandkon/product/ProductRepository.java
@@ -1,11 +1,19 @@
 package practice.brandkon.product;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Product> findById(Long id);
+
     List<Product> findAllByOrderBySalesDesc();
+
     List<Product> findAllByBrandId(Long brandId);
+
     List<Product> findAllByBrandIdOrderBySalesDesc(Long brandId);
 }
